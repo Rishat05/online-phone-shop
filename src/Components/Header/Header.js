0c602019c6304a5613ab2676b083c7bd0a34a36f
rishat <rishatrifa56@gmail.com>
2022-03-26 23:05:06 +0600

    title change

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import "./Header.css"
 const Header = () => {
     return (
         <div className='header'>
-            <h1>Online Mobile Shop</h1>
+            <h1>Online Phone Shop</h1>
             <p>Choose the 4 Phones</p>
         </div>
     );
